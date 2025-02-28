85975f7a74ccd2be79c38011d0ea2ae2310dced9
Yaminianitha19 <yaminianitha22@gmail.com>
2025-02-28 14:00:20 +0530

    create operation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -34,25 +34,25 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id']=randrange(0,1000000)
     my_posts.append(post_dict)
     return {"data":post_dict}
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return {"detail": post}
 
 
+
+    
 @app.get("/posts/{id}")
 def get_post(id : int):
-    
-    
+
     post = find_post(id)
-    print(post)
-    return{"post_detail":post}
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id:{id} was not found ")
+       
+    return {"post_detail": post}
+
 
 
 
