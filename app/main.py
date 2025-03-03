0855f66c18b65364ad2a4c9d6733fac67fbbcce3
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-03 10:55:24 +0530

    5:45 mins covered

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from sqlalchemy.sql.functions import mode
+from . import models, schemas
 from .database import engine, get_db
 
 
@@ -17,10 +18,6 @@ app = FastAPI()
 
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published : bool = True
 
 while True:
     try:
@@ -58,26 +55,16 @@ def find_index_post(id):
 def root():
     return {"message": "Welcome to FastAPI!!!"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-
-    posts =db.query(models.Post).all()
-    
-    print(posts)
-    return{"data": "successfully"}
-
-
-
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = 1""")
     # posts= cursor.fetchall()
      posts =db.query(models.Post).all()
-     return {"data": posts}
+     return  posts
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     # (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -88,7 +75,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data":new_post}
+    return new_post
 
 
 
@@ -121,7 +108,7 @@ def delete_post(id:str, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id : int, post: Post, db: Session = Depends(get_db)):
+def update_post(id : int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     #  cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,(post.title, post.content, post.published, str(id)))
     #  updated_post = cursor.fetchone()
     #  conn.commit()
@@ -131,8 +118,8 @@ def update_post(id : int, post: Post, db: Session = Depends(get_db)):
     
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    post_query.update(post.model_dump(),synchronize_session=False)
+    post_query.update(updated_post.model_dump(),synchronize_session=False)
     
     db.commit()
-    return {'data': post_query.first()}
+    return post_query.first()
 
