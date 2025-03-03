178d5a58a297b56a78e1a9eaa44d3f27a4f548b5
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-03 16:24:29 +0530

    created new seperate files for routers

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import mode
 from . import models, schemas, utils
 from .database import engine, get_db
+from . routers import post, user
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -36,7 +37,6 @@ while True:
 my_posts = [{"title": "title of post 1", "content": "content of post 1","id": 1},{"title":"favo foods","content":"i like pizza","id":2}]
 
 
-
 def find_post(id):
     for p in my_posts:
         if p['id'] == id:
@@ -47,104 +47,12 @@ def find_index_post(id):
         if p['id'] == id:
             return i
 
-    return None
-
-
+app.include_router(post.router)
+app.include_router(user.router)
 
 @app.get("/")
 def root():
     return {"message": "Welcome to FastAPI!!!"}
-
-@app.get("/posts", response_model = list[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = 1""")
-    # posts= cursor.fetchall()
-     posts =db.query(models.Post).all()
-     return  posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model = schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-    # (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-
-    # conn.commit()
-   
-    new_post =models.Post(**post.model_dump())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-
-
-    
-@app.get("/posts/{id}", response_model = schemas.Post)
-def get_post(id : int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id)))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id:{id} was not found ")
-       
-    return post
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT )
-def delete_post(id:str, db: Session = Depends(get_db)):
-
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(str(id)))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    post.delete(synchronize_session = False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", response_model = schemas.Post)
-def update_post(id : int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    #  cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,(post.title, post.content, post.published, str(id)))
-    #  updated_post = cursor.fetchone()
-    #  conn.commit()
-    post_query= db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    db.commit()
-    
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    post_query.update(updated_post.model_dump(),synchronize_session=False)
-    
-    db.commit()
-    return post_query.first()
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model = schemas.UserOut)
-def create_user(user : schemas.UserCreate, db: Session = Depends(get_db)):
-     
-
-
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    
-    new_user =models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@app.get('/users/{id}',response_model = schemas.UserOut)
-def get_user(id : int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"user with id:{id} was not found ")
-       
-    return user
 
 
 
