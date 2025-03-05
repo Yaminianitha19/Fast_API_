d07d65641422cfe99e146f80b1f04da9d93518d3
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-05 19:29:31 +0530

    10:20

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List, Optional
-
+from sqlalchemy import func
 router = APIRouter(
     prefix="/posts",
     tags=['Posts']
@@ -16,6 +16,8 @@ limit : int = 10, skip: int = 0, search :Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts """)
     # posts= cursor.fetchall()
      posts =db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
+     results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote)
      return  posts
 
 
