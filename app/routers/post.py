516828eeebe9123f48f856c2ec4c64910e2237d5
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-05 12:10:19 +0530

    added limit

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,10 +10,12 @@ router = APIRouter(
 )
 
 @router.get("/", response_model = List[schemas.Post])
-def get_posts(db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user),
+limit : int = 10):
+     print(limit)
     # cursor.execute("""SELECT * FROM posts """)
     # posts= cursor.fetchall()
-     posts =db.query(models.Post).all()
+     posts =db.query(models.Post).limit(limit).all()
      return  posts
 
 
