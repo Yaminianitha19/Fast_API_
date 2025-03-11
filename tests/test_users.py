babe7cca1d9e60f7fe4fb926c0bc4f596fb67084
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-12 00:17:44 +0530

    16:28:02

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,13 @@
+import pytest
+from jose import jwt
 from app import schemas 
-from .database import client, session
+from app.config import settings
 
-def test_root(client, session):
-    
-    res = client.get("/")
-    print(res.json().get("message"))
-    assert res.json().get("message") == "Happy Coding! :)" 
-    assert res.status_code == 200
-
+# def test_root(client, session):
+#     res = client.get("/")
+#     print(res.json().get("message"))
+#     assert res.json().get("message") == "Happy Coding! :)" 
+#     assert res.status_code == 200
 
 def test_create_user(client):
     res = client.post("/users/", json={"email": "a@b.com", "password": "password123"})
@@ -15,7 +15,29 @@ def test_create_user(client):
     assert new_user.email == "a@b.com"
     assert res.status_code == 201
 
-def test_login_user(client):
-    res = client.post("/login", data={"username": "a@b.com", "password": "password123"})
-    print(res.json())
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code",[
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('a@b.com', 'wrongpassword', 403)])
+def test_incorrect_login(client, test_user, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
+    assert res.json().get('detail') == "invalid credentials"
+
+def test_missing_username(client):
+    res = client.post("/login", data={"password": "password123"})
+    assert res.status_code == 422
+
+def test_missing_password(client):
+    res = client.post("/login", data={"username": "a@b.com"})
+    assert res.status_code == 422
