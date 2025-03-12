b91346bd8468bf362d5f246584fc6a4c0c3deac8
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-12 09:11:34 +0530

    17:07:49

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ from app.main import app
 from app.config import settings
 from app.database import get_db
 from app.database import Base
+from app.oauth2 import create_access_token
+from app import models
 from alembic import command 
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:2004@localhost:5432/fastapi_test' 
@@ -61,3 +63,62 @@ def test_user(client):
     new_user = res.json()
     new_user["password"] = user_data["password"]
     return new_user
+
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {
+        "email": "user2@example.com",
+        "password": "password123"
+    }
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+
+
+@pytest.fixture
+def token(test_user): 
+    return create_access_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "user2 post",
+        "content": "this is user2's post",
+        "owner_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+    
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    session.commit()
+    
+    posts = session.query(models.Post).all()
+    return posts
